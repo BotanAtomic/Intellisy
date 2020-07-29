69047ee6ae9c4a27af31ac4733cf12e4da03ed7c
botan <botan.david94@gmail.com>
2020-07-29 22:09:59 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "com.botan"
+version = "1.0"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("org.nd4j:nd4j-cuda-10.1-platform:1.0.0-beta7")
+    implementation("org.nd4j:nd4j-api:1.0.0-beta7")
+    implementation("org.datavec:datavec-data-image:1.0.0-beta7")
+    implementation("org.deeplearning4j:deeplearning4j-cuda-10.1:1.0.0-beta7")
+    implementation("org.deeplearning4j:deeplearning4j-ui:1.0.0-beta7")
+    implementation("org.deeplearning4j:deeplearning4j-zoo:1.0.0-beta7")
+    implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("org.knowm.xchart:xchart:3.6.4")
+}
