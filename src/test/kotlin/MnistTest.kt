33d19bd72672a3060d4d1568a8898706885c9697
botan <botan.david94@gmail.com>
2020-08-02 00:29:40 +0200

    Add some SimpleCNN to Mnist test (48*48)

--- src/test/kotlin/MnistTest.kt
+++ src/test/kotlin/MnistTest.kt
@@ -2,23 +2,56 @@ import intellisy.configuration.ClassifierConfiguration
 import intellisy.core.ImageClassifier
 import intellisy.dataset.Dataset
 import intellisy.image.ImageFormat
+import intellisy.models.SimpleCNNModel
 import intellisy.models.SmallCNNModel
+import org.junit.Assert.assertTrue
+import org.junit.Test
 import java.io.File
+import kotlin.test.assertNotNull
 
-fun main() {
-    val classifier = ImageClassifier(
-        dataset = Dataset.fromFolder(
-            trainFolder = File("dataset/mnist/train"),
-            testFolder = File("dataset/mnist/test")
-        ),
-        configuration = ClassifierConfiguration(
-            width = 28,
-            height = 28,
-            format = ImageFormat.GRAYSCALE
-        ),
-        model = SmallCNNModel()
-    )
+class MnistTest {
 
-    classifier.train { evaluation -> println(evaluation.stats()) }
+    @Test
+    fun mnistTestSmallCNN() {
+        val classifier = ImageClassifier(
+            dataset = Dataset.fromFolder(
+                trainFolder = File("dataset/mnist/train"),
+                testFolder = File("dataset/mnist/test")
+            ),
+            configuration = ClassifierConfiguration(
+                width = 28,
+                height = 28,
+                format = ImageFormat.GRAYSCALE
+            ),
+            model = SmallCNNModel()
+        )
+
+        val testEval = classifier.train { validationEval -> println(validationEval.stats()) }
+
+        assertNotNull(testEval)
+        assertTrue(testEval.accuracy() > 0.992)
+    }
+
+    @Test
+    fun mnistTestSimpleCNN() {
+        val classifier = ImageClassifier(
+            dataset = Dataset.fromFolder(
+                trainFolder = File("dataset/mnist/train"),
+                testFolder = File("dataset/mnist/test")
+            ),
+            configuration = ClassifierConfiguration(
+                width = 48,
+                height = 48,
+                format = ImageFormat.GRAYSCALE,
+                epochs = 15
+            ),
+            model = SimpleCNNModel()
+        )
+
+        val testEval = classifier.train { validationEval -> println(validationEval.stats()) }
+
+        assertNotNull(testEval)
+        assertTrue(testEval.accuracy() >= 0.99)
+    }
 
 }
