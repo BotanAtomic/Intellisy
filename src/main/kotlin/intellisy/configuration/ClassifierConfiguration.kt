77d08292c41fedbb3a82a37ab9662a36bd92c995
botan <botan.david94@gmail.com>
2020-08-03 22:16:39 +0200

    Rename some var

--- src/main/kotlin/intellisy/configuration/ClassifierConfiguration.kt
+++ src/main/kotlin/intellisy/configuration/ClassifierConfiguration.kt
@@ -9,18 +9,19 @@ import org.deeplearning4j.nn.conf.layers.ConvolutionLayer
 import java.security.SecureRandom
 
 data class ClassifierConfiguration(
-        val width: Long = 28,
-        val height: Long = 28,
-        val format: ImageFormat = ImageFormat.GRAYSCALE,
-        val allowedFormats: List<String> = NativeImageLoader.ALLOWED_FORMATS.toList(),
-        val imageTransformation: ImageTransformation = ImageTransformation(),
+    val width: Long = 28,
+    val height: Long = 28,
+    val format: ImageFormat = ImageFormat.GRAYSCALE,
+    val allowedFormats: List<String> = NativeImageLoader.ALLOWED_FORMATS.toList(),
+    val dataAugmentation: ImageTransformation = ImageTransformation(),
 
-        val batchSize: Int = 32,
-        val epochs: Int = 10,
-        val seed: Long = SecureRandom().nextLong(),
-        val validationSplit: Double = 0.2,
 
-        val cacheMode: CacheMode = CacheMode.DEVICE,
-        val cudnnAlgoMode: ConvolutionLayer.AlgoMode = ConvolutionLayer.AlgoMode.PREFER_FASTEST,
-        val workspaceMode: WorkspaceMode = WorkspaceMode.ENABLED
+    val batchSize: Int = 32,
+    val epochs: Int = 10,
+    val seed: Long = SecureRandom().nextLong(),
+    val validationSplit: Double = 0.2,
+
+    val cacheMode: CacheMode = CacheMode.DEVICE,
+    val cudnnAlgoMode: ConvolutionLayer.AlgoMode = ConvolutionLayer.AlgoMode.PREFER_FASTEST,
+    val workspaceMode: WorkspaceMode = WorkspaceMode.ENABLED
 )
