ae2662214f95e00a1bb6d269ebdf071bd7946fc4
botan <botan.david94@gmail.com>
2020-08-03 22:15:46 +0200

    Optimize ImageTransformation.kt pipeline, getting by lazy

--- src/main/kotlin/intellisy/image/ImageTransformation.kt
+++ src/main/kotlin/intellisy/image/ImageTransformation.kt
@@ -5,11 +5,13 @@ import org.datavec.image.transform.PipelineImageTransform
 import org.nd4j.common.primitives.Pair
 
 class ImageTransformation(private val shuffle: Boolean = true) {
-    private val pipeline = ArrayList<Pair<ImageTransform, Double>>()
 
-    fun add(transformation: ImageTransform, probability: Double): ImageTransformation {
+    private val pipeline: ArrayList<Pair<ImageTransform, Double>> by lazy {
+        ArrayList<Pair<ImageTransform, Double>>()
+    }
+
+    fun add(transformation: ImageTransform, probability: Double = 1.0) {
         pipeline.add(Pair(transformation, probability))
-        return this
     }
 
     fun buildPipeline() = PipelineImageTransform(pipeline, shuffle)
